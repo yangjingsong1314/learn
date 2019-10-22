ac6294fcd174c1f4896d62bf9053d8df5a9acc23
yangjingsong1314 <1376318110@qq.com>
2019-10-22 15:04:42 +0800

    1

--- guest_master/run.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from util.utils import CASEPATH,REPORTPATH,get_time
-from HTMLTestReportCN import HTMLTestRunner
-
-tests = unittest.defaultTestLoader.discover(CASEPATH)
-with open(REPORTPATH+'/'+get_time()+'.html','wb') as f:
-    runner = HTMLTestRunner(stream=f,title='guest-master接口测试报告',tester='杨劲松')
-    runner.run(tests)
