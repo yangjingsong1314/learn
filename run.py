84e122d80436e9eb8471ceb26c85f6aaf663a1c2
yangjingsong1314 <1376318110@qq.com>
2019-10-22 16:49:47 +0800

    change run.py

--- run.py
+++ run.py
@@ -6,3 +6,6 @@ tests = unittest.defaultTestLoader.discover(CASEPATH)
 with open(REPORTPATH+'/'+get_time()+'.html','wb') as f:
     runner = HTMLTestRunner(stream=f,title='guest-master接口测试报告',tester='杨劲松')
     runner.run(tests)
+
+
+print('************************************************')
